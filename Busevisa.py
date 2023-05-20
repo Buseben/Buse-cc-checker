efbea779cea5e4ca2a730ca9950ede67285f13db
Buseben <133596191+Buseben@users.noreply.github.com>
2023-05-21 00:37:08 +0300

    Add files via upload

---
+++ Busevisa.py
@@ -0,0 +1,80 @@
+import random
+from enum import Enum
+ 
+class Card(Enum):
+    Visa = 0
+    Mastercard = 1
+
+ 
+def computeCheckDigit(cardNumber):
+    total = 0
+    digits = cardNumber[::-1]
+    for i in range(len(digits)):
+        digit = int(digits[i])
+        if i % 2 == 0:
+            digit *= 2
+        total += sum(map(int, str(digit)))
+    return (total * 9) % 10
+ 
+def fillToLength(text, length):
+    toAdd = length - len(text)
+    for _ in range(toAdd):
+        text += str(random.randint(0, 9))
+    return text
+ 
+def genCardNumber(cardType):
+    if cardType == Card.Visa:
+        length = 16
+        prefix = str(4)
+    elif cardType == Card.Mastercard:
+        length = 16
+        prefix = str(random.randint(51, 55))
+    else:
+        raise Exception("Invalid card type!")
+    cardNumber = fillToLength(prefix, length - 1)
+    return cardNumber + str(computeCheckDigit(cardNumber))
+ 
+
+def generateCVV():
+   am = random.randint(0, 9)
+   yarak = random.randint(0, 9)
+   amcık = random.randint(0, 9)
+   cvv = str(am) + str(yarak) + str(amcık)
+   return cvv
+
+
+def generateEXP():
+	ay = random.randint(1,12)
+	yıl = random.randint(21,35)
+	if ay < 10:
+		ay = f"0{ay}"
+	return f"{ay}|20{yıl}"
+print ("""\033[91m▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+▀ ▀
+▀ B U S E   ▀ 
+▀ ▀
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀          """)
+
+cart_type = input("\033[92mKart Tipini Seç Knk. (visa/master) => ").lower()
+count = int(input("\033[93mKaç tane card üretilsin knk? (1,2,500,10000) => "))
+
+if cart_type == "visa":
+	with open('/sdcard/download/ccler.txt', 'a') as f:
+		    f.write("ID      CART      CARD NUMBER      SKT      CVV\n\n")
+		    for x in range(0,count):
+		        f.write(f"{genCardNumber(Card.Visa)}|{generateEXP()}|{generateCVV()}\n")
+		
+		        print(f"{genCardNumber(Card.Visa)} | {generateEXP()} | {generateCVV()}")
+
+
+elif cart_type == "master":
+	with open('/sdcard/download/ccler.txt', 'a') as f:
+		    f.write("ID      CART      CARD NUMBER      SKT      CVV\n\n")
+		    for x in range(0,count):
+		        f.write(f"{genCardNumber(Card.Visa)}|{generateEXP()}|{generateCVV()}\n")
+		
+		        print(f"{genCardNumber(Card.Mastercard)} | {generateEXP()} | {generateCVV()}")
+
+    
+else:
+    print("geçersiz kart  (VİSA/MASTER)")
